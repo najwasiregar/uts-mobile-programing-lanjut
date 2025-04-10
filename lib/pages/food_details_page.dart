a61c58d4e6f5ecc500db3a1b9da5a2ee3ca0ce7c
najwasiregar <118334792+najwasiregar@users.noreply.github.com>
2025-04-10 21:56:31 +0700

    menambahka sesi rating dan rating number

--- lib/pages/food_details_page.dart
+++ lib/pages/food_details_page.dart
@@ -23,18 +23,47 @@ class _FoodDetailsPageState extends State<FoodDetailsPage> {
         children: [
           // listview of food details
           Expanded(
-            child: ListView(
-              children: [
-                // image 
-                
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal:25.0),
+              child: ListView(
+                children: [
+                  // image 
+                  Image.asset(widget.food.imagePath,
+                  height: 200,
+                  ),
+              
+                  const SizedBox(height: 25,
+                  ),
+              
+                  // rating
+                  Row(
+                    children: [
+                      // star icon
+                      Icon(Icons.star, color: Colors.yellow[800],
+                      ),
+              
+                     const SizedBox(height: 5),
+              
+                      // rating number
+                      Text(
+                        widget.food.rating,
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
 
-                // rating
-
-                // food name
-
-                // description
-
-              ],
+                  const SizedBox(height: 10),
+              
+                  // food name
+              
+              
+                  // description
+              
+                ],
+              ),
             ),
           ),
 
